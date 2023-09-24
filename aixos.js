f5e2028a7d3213961e8f4fbf666e3cdd08b8dcab
CongLinh1003 <01633007559mc@gmail.com>
2023-09-24 20:28:59 +0700

    axiosss

---
+++ aixos.js
@@ -0,0 +1,13 @@
+axios({
+
+    method: 'get',
+    
+    url: 'https://jsonplaceholder.typicode.com/todos/1',
+    
+    })
+    
+    .then(function (response) {
+    
+    console.log(response);
+    
+    });
--- fetchjs.js
+++ fetchjs.js
@@ -2,65 +2,65 @@
 
 
 
-// fetch('https://jsonplaceholder.typicode.com/todos')
-//     .then(response => response.json())
-//     .then(
-//         (data) => {
-//             var htmls = data.map(
-//                 function (posts) {
-//                     return `<li> 
-//                             <h2>${posts.id}</h2>
-//                             <h2>${posts.title}</h2>
-//                             </li>`
-//                 }
-//             );
-//             var html = htmls.join();
-//             var text = document.getElementById('info').innerHTML
-//                 = html;
-//         }
-//     )
-//     .catch(() => console.log('Có lỗi!'))
+fetch('https://jsonplaceholder.typicode.com/todos')
+    .then(response => response.json())
+    .then(
+        (data) => {
+            var htmls = data.map(
+                function (posts) {
+                    return `<li> 
+                            <h2>${posts.id}</h2>
+                            <h2>${posts.title}</h2>
+                            </li>`
+                }
+            );
+            var html = htmls.join();
+            var text = document.getElementById('info').innerHTML
+                = html;
+        }
+    )
+    .catch(() => console.log('Có lỗi!'))
 
-// fetch('https://jsonplaceholder.typicode.com/posts/1')
+fetch('https://jsonplaceholder.typicode.com/posts/1')
 
-// .then((response) => response.json())
+.then((response) => response.json())
 
-// .then((json) => console.log(json));
+.then((json) => console.log(json));
 
-// fetch('https://jsonplaceholder.typicode.com/posts')
+fetch('https://jsonplaceholder.typicode.com/posts')
 
-// .then((response) => response.json())
+.then((response) => response.json())
 
-// .then((json) => console.log(json));
+.then((json) => console.log(json));
 
 
-// fetch('https://jsonplaceholder.typicode.com/posts/1', {
+fetch('https://jsonplaceholder.typicode.com/posts/1', {
 
-//     method: 'PUT',
+    method: 'PUT',
 
-//     body: JSON.stringify({
+    body: JSON.stringify({
 
-//         id: 1,
+        id: 1,
 
-//         title: 'foo',
+        title: 'foo',
 
-//         body: 'bar',
+        body: 'bar',
 
-//         userId: 1,
+        userId: 1,
 
-//     }),
+    }),
 
-//     headers: {
+    headers: {
 
-//         'Content-type': 'application/json; charset=UTF-8',
+        'Content-type': 'application/json; charset=UTF-8',
 
-//     },
+    },
 
-// })
+})
 
-//     .then((response) => response.json())
+    .then((response) => response.json())
 
-//     .then((json) => console.log(json));
+    .then((json) => console.log(json));
 
 fetch('https://jsonplaceholder.typicode.com/posts', {
 
